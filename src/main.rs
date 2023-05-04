95e35d1b93c029aead99e8dc6ef5391476643547
PitchBlackNights <pitchblacknights@proton.me>
2023-05-04 17:17:38 -0500

    slight formatting and fixes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
+#![no_std]
+#![no_main]
+#![no_core]
 #![crate_type="rlib"]
 #![feature(no_core)]
 #![feature(lang_items)]
-#![no_main]
-#![no_std]
-#![no_core]
 
 
-//#[lang="sized"]
-//trait Sized {}
+// Weird rustc error here, anyone know how to fix it?
+// FULL ERROR:
+// error[E0152]: found duplicate lang item `sized`
+//   --> src\main.rs:10:1
+//    |
+// 20 | trait Sized {}
+//    | ^^^^^^^^^^^
+//    |
+//    = note: the lang item is first defined in crate `core` (which `std` depends on)
+//    = note: first definition in `core` loaded from \\?\C:\Users\[REDACTED]\.rustup\toolchains\nightly-x86_64-pc-windows-msvc\lib\rustlib\x86_64-pc-windows-msvc\lib\libcore-798bc7c2d986ac97.rlib
+//    = note: second definition in the local crate (`lute`)
+#[lang="sized"]
+trait Sized {}
 
 #[no_mangle] 
 fn _start() -> ! {
