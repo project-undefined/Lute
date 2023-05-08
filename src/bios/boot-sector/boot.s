2950db6adae8c93c8c4f9aa26441efd4f960ae26
Wesley Schell <wesley.schell.sh@proton.me>
2023-05-08 12:42:40 -0500

    Added comments

--- src/bios/boot-sector/boot.s
+++ src/bios/boot-sector/boot.s
@@ -4,15 +4,20 @@
 
 
 _start: 
-    cli
+    cli ; clear interrupts, don't want this code to be interrupted
+    ; allows bootloader to access all parts of memory by making the segment registers equal
+    ; TODO: will probably need to be changed when moved to ARM64
     MOV %cs, %rax
     MOV %rax, %ds
     MOV %rax, %es
-    MOV %rax, %ss
-    MOV $0x7c00, %rbp
+    MOV %rax, %ss 
+    ; set the base and stack pointers to the correct memory location required
+    ; for bootloader applications
+    MOV $0x7c00, %rbp 
     MOV $0x7c00, %rsp
-    sti
-
+    sti ; start interrupts
+    
+    ; call the bootmain function
     call _boot_main
    
     hlt
